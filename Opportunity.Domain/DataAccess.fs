26f8b7542977e592125ece9e4062f41af187621e
Dave Warry <dave@warry.org.uk>
2017-04-01 06:36:48 +0100

    Move Initiative functions to separate classes/modules

--- Opportunity.Domain/DataAccess.fs
+++ Opportunity.Domain/DataAccess.fs
@@ -15,8 +15,8 @@ let internal sqlFolder = __SOURCE_DIRECTORY__ + "\\SQL"
 
 let internal connectionString = System.Configuration.ConfigurationManager.ConnectionStrings.["Opportunity"].ConnectionString
 
-type private DB = SqlProgrammabilityProvider<"name=Opportunity", 
-                                             ConfigFile="DesignTime.config">
+type internal DB = SqlProgrammabilityProvider<"name=Opportunity", 
+                                              ConfigFile="DesignTime.config">
 
 
 type private GetCategories = SqlCommandProvider<"GetCategories.sql",
@@ -34,28 +34,6 @@ type private GetOrgUnits = SqlCommandProvider<"GetOrgUnits.sql",
 type GetOrgUnitsRecord = GetOrgUnits.Record
 
 
-type private GetActiveInitiatives = SqlCommandProvider<"GetActiveInitiatives.sql",
-                                                       "name=Opportunity",
-                                                       ConfigFile="DesignTime.config",
-                                                       ResolutionFolder=sqlFolder>
-
-type GetActiveInitiativesRecord = GetActiveInitiatives.Record
-
-
-
-type private CreateInitiative = SqlCommandProvider<"CreateInitiative.sql",
-                                                   "name=Opportunity",
-                                                   ConfigFile="DesignTime.config",
-                                                   ResolutionFolder=sqlFolder,
-                                                   SingleRow=true>
-
-type CreateInitiativeRecord = CreateInitiative.Record
-
-type private DeleteInitiative = SqlCommandProvider<"DeleteInitiative.sql",
-                                                   "name=Opportunity",
-                                                   SingleRow=true,
-                                                   ConfigFile="DesignTime.config",
-                                                   ResolutionFolder=sqlFolder >
 
 
 
@@ -80,69 +58,5 @@ let getOrgUnits (): GetOrgUnitsRecord[] =
                                  use cmd = new GetOrgUnits(conn, transaction = tran)
                                  (true, cmd.Execute () |> Seq.toArray ) )
 
-let getActiveInitiatives (pageSize: int) (pageIndex: int) (asAt: DateTime): GetActiveInitiativesRecord[] = 
-    doInTransaction IsolationLevel.ReadCommitted
-                    (fun tran -> let conn = tran.Connection
-                                 use cmd = new GetActiveInitiatives(conn, transaction = tran)
-                                 (true, cmd.Execute (pageSize, pageIndex, asAt.Date) |> Seq.toArray))
 
 
-
-              
-    
-    
-let createInitiative (name: string) 
-                     (description: string)
-                     (link: string)
-                     (logoUrl: string)
-                     (startDate: DateTime)
-                     (endDate: DateTime)
-                     (updatedBy: string)
-                     (orgUnitId: int) : CreateInitiativeRecord option =
-    doInTransaction IsolationLevel.ReadCommitted
-                    (fun tran -> let conn = tran.Connection
-                                 use cmd = new CreateInitiative(conn, transaction=tran)
-                                 let newId = cmd.Execute(name, 
-                                                         description, 
-                                                         link, 
-                                                         logoUrl, 
-                                                         startDate, 
-                                                         endDate, 
-                                                         updatedBy, 
-                                                         orgUnitId)
-
-                                 match newId with
-                                 | Some y -> (true, newId)
-                                 | _      -> (false, None))
-
-let deleteInitiative (id: int) = 
-    doInTransaction IsolationLevel.ReadCommitted
-                    (fun tran -> let conn = tran.Connection
-                                 use cmd = new DeleteInitiative(conn, transaction = tran)
-                                 let result = cmd.Execute(id)
-                                 match result with
-                                 | Some (Some x) -> (true, Some x)
-                                 | _             -> (false, None))
-
-
-
-type private GetAllInitiatives = SqlCommandProvider<"GetAllInitiatives.sql", "name=Opportunity", ConfigFile="DesignTime.config", ResolutionFolder=sqlFolder >
-
-type private GetAllInitiativesRecord = GetAllInitiatives.Record
-
-let getAllInitiatives (pageSize: int) (pageIndex: int): GetAllInitiativesRecord[] = 
-    doInTransaction IsolationLevel.ReadCommitted
-                    (fun tran -> let conn = tran.Connection
-                                 use cmd = new GetAllInitiatives(conn, transaction = tran)
-                                 (true, cmd.Execute(pageSize, pageIndex) |> Seq.toArray ) )
-
-
-type private GetInitiative = SqlCommandProvider<"GetInitiative.sql", "name=Opportunity", SingleRow=true, ConfigFile="DesignTime.config", ResolutionFolder=sqlFolder >
-
-type private GetInitiativeRecord = GetInitiative.Record
-
-let getInitiative (id: int): GetInitiativeRecord option = 
-    doInTransaction IsolationLevel.ReadCommitted
-                    (fun tran -> let conn = tran.Connection
-                                 use cmd = new GetInitiative(conn, transaction = tran)
-                                 (true, cmd.Execute (id) ) )
