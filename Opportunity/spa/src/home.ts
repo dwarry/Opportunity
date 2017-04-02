6e33aeb2eefb9a61bae51bbe5c7bb0537bde2ac8
Dave Warry <dave@warry.org.uk>
2017-04-02 06:11:44 +0100

    retrieves current user details in Home view model.

--- Opportunity/spa/src/home.ts
+++ Opportunity/spa/src/home.ts
@@ -1,5 +1,17 @@
-export class Home {
-    activate() {
+import { autoinject } from 'aurelia-framework';
+import { DataService } from './dataService';
+import { IUser } from './models/user'
 
+
+@autoinject
+export class Home {
+    user: IUser = null;
+
+    constructor(private _dataService: DataService) {
+
+    }
+
+    activate() {
+        return this._dataService.getUser().then(u => this.user = u);
     }
 }
